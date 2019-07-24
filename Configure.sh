fd41ddfcbdd7eb44d25a7d4a0f13eb8d4e61cb2b
Marcelo Marques <marcelo@smarques.com>
2019-07-24 18:47:17 -0300

    First Commit

--- Configure.sh
+++ Configure.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 # Build Image
-docker build . -t centos6-cm:1
+docker build . -t 'cm:1'
 
 # Stop/Delete All Existent Containers
 if [ `docker ps | grep -v CONTAINER | wc -l` != 0 ]; then
     docker stop `docker ps | grep -v CONTAINER | awk '{print $1}'`
     sleep 5
 fi
+if [ `docker images | grep -v REPOSITORY | wc -l` != 0 ]; then
+    docker image rm `docker images | grep -v REPOSITORY | awk '{print $3}'`
+    sleep 5
+fi
+
+docker image rm centos:6
 
 # Start Containers (Check MySQL Pass)
-docker run -d -it -h cm01 --name cloudera-cm --rm -p 7180:7180 centos:6
+docker run -d -it -h cm01 --name cloudera-cm --rm -p 7180:7180 cm:1
 docker run -d -it -h db01 --name cloudera-db --rm -e MYSQL_ROOT_PASSWORD=passw0rd mysql:latest --default-authentication-plugin=mysql_native_password
 
 # SSH/Hosts Conf
-docker exec cloudera-cm bash -c "/etc/init.d/ssh start"
-docker exec cloudera-db bash -c "/etc/init.d/ssh start"
+docker exec cloudera-cm bash -c "/etc/init.d/sshd start"
+docker exec cloudera-db bash -c "/etc/init.d/sshd start"
 docker exec cloudera-cm bash -c "ssh-keygen -q -t rsa -N '' -f /root/.ssh/id_rsa && cat /root/.ssh/id_rsa.pub > /root/.ssh/authorized_keys2"
 docker exec cloudera-cm bash -c "echo '$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cloudera-db)' db01 >> /etc/hosts"
 
--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,9 @@ LABEL maintainer="Marcelo Marques <marcelo@smarques.com>"
 
 EXPOSE 7180
 
-RUN yum update && yum install -y vim wget iputils
+RUN yum update -y && yum install -y vim wget iputils openssh-server openssh-clients
 RUN mkdir -p /usr/share/java
-ADD https://archive.cloudera.com/cm6/6.2.0/redhat6/yum/cloudera-manager.repo -P /etc/yum.repos.d/
-COPY https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.46.tar.gz /usr/share/java
+ADD https://archive.cloudera.com/cm6/6.2.0/redhat6/yum/cloudera-manager.repo /etc/yum.repos.d/
+ADD https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.46.tar.gz /usr/share/java
+RUN tar -xzf /usr/share/java/mysql-connector-java-5.1.46.tar.gz --directory=/usr/share/java/ && mv /usr/share/java/mysql-connector-java-5.1.46/mysql-connector-java-5.1.46-bin.jar /usr/share/java/mysql-connector-java.jar && rm -f /usr/share/java/mysql-connector-java-5.1.46.tar.gz && rm -fr /usr/share/java/mysql-connector-java-5.1.46
 RUN yum install -y oracle-j2sdk1.8 cloudera-manager-daemons cloudera-manager-agent cloudera-manager-server mysql
